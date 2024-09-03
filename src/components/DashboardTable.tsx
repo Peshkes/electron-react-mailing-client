ec254a4f4ba38dd7a9ad6b276aba122783c6603d
Andrey Dyuzhikov <146117567+Anhair32167@users.noreply.github.com>
2024-09-03 18:01:34 +0300

    Dyuzhikov. Dashboard animations added

--- src/components/DashboardTable.tsx
+++ src/components/DashboardTable.tsx
@@ -11,54 +11,88 @@ const DashboardTable = (props: Props) => {
         <div className="w-full h-full flex flex-col justify-start bg-white border-4 border-solid border-cyan-800/20 rounded-2xl">
             <div className="w-full py-5  flex justify-between border-b-4 border-solid border-cyan-800/20">
                 <p className="pl-7 text-2xl text-cyan-800">{props.title}</p>
-                {props.plus ? <div className="flex items-center pr-7"><Plus color="black"/></div> : <></>}
+                {props.plus ? <div className="flex items-center pr-7 cursor-pointer"><Plus color="black"/></div> : <></>}
             </div>
-            <div className="w-full pb-5 px-7 overflow-auto scroll-smooth scrollbar-none text-cyan-800">
-                <div className="w-full py-5 grid grid-cols-2 font-bold sticky top-0 bg-white">
+            <div className="w-full pb-5 overflow-auto scroll-smooth scrollbar-none text-cyan-800">
+                <div className="w-full py-5 px-7 grid grid-cols-2 font-bold sticky top-0 bg-white">
                     <div>Имя</div>
                     <div>Телефон</div>
                 </div>
                 <div className="w-full">
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
-                    <div className="grid grid-cols-2 pt-1 ">
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                         <div>Иванов Иван Иванович</div>
                         <div>89159999999</div>
                     </div>
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
+                        <div>Иванов Иван Иванович</div>
+                        <div>89159999999</div>
+                    </div>
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
+                        <div>Иванов Иван Иванович</div>
+                        <div>89159999999</div>
+                    </div>
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
+                        <div>Иванов Иван Иванович</div>
+                        <div>89159999999</div>
+                    </div>
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
+                        <div>Иванов Иван Иванович</div>
+                        <div>89159999999</div>
+                    </div>
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
+                        <div>Иванов Иван Иванович</div>
+                        <div>89159999999</div>
+                    </div>
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
+                        <div>Иванов Иван Иванович</div>
+                        <div>89159999999</div>
+                    </div>
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
+                        <div>Иванов Иван Иванович</div>
+                        <div>89159999999</div>
+                    </div>
+                    <div className="px-7 grid grid-cols-2 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
+                        <div>Иванов Иван Иванович</div>
+                        <div>89159999999</div>
+                    </div>
+
+
 
                 </div>
                 {/*<table className="w-full h-full text-left text-cyan-800">*/}
