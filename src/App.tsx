ddca0800684bd3afff1add5a38a834793568285f
peshkes <peshkesv@gmail.com>
2024-09-04 21:34:50 +0400

    Fixed router

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,22 @@ import Header from "./components/Header";
 import {Route, Routes} from "react-router-dom";
 
 import ClientsPage from "./pages/ClientsPage";
-import ClientPage from "./pages/ClientPage";
 import MessagesPage from "./pages/MessagesPage";
-import MessagePage from "./pages/MessagePage";
 import DashboardPage from "./pages/DashboardPage";
+import {ChildWindowProvider} from "./components/child-window/ChildWindowProvider";
+
 
 const App = () => {
-
     return (
-        <div className={'App'}>
-            <div className="container bg-cyan-800/5 mx-auto flex flex-col justify-start h-screen h-max-screen ">
-                <Header/>
-                <DashboardPage/>
-                {/*<Routes>*/}
-                {/*    <Route path={"/"} element={<DashboardPage/>}/>*/}
-                {/*    <Route path={"/clients"} element={<ClientsPage/>}/>*/}
-                {/*    <Route path={"/client/:id"} element={<ClientPage/>}/>*/}
-                {/*    <Route path={"/messages"} element={<MessagesPage/>}/>*/}
-                {/*    <Route path={"/message/:id"} element={<MessagePage/>}/>*/}
-                {/*</Routes>*/}
-
-            </div>
+        <div className={'App bg-cyan-800/5 h-screen h-max-screen flex flex-col'}>
+            <Header/>
+            <ChildWindowProvider>
+                <Routes>
+                    <Route path={"/"} element={<DashboardPage/>}/>
+                    <Route path={"/clients"} element={<ClientsPage/>}/>
+                    <Route path={"/messages"} element={<MessagesPage/>}/>
+                </Routes>
+            </ChildWindowProvider>
         </div>
     );
 };
