826528fa76dae3b7b77ee439d59f6271b5d7aeb7
peshkes <peshkesv@gmail.com>
2024-09-16 22:44:23 +0400

    Forgotten change from the last commit

--- src/components/tables/TemplateMessages.tsx
+++ src/components/tables/TemplateMessages.tsx
@@ -2,38 +2,26 @@ import React, {useContext, useEffect, useState} from 'react';
 import {SampleMessage} from "../../api/types";
 import {getAllSampleMessages} from "../../api/fake";
 import {ChildWindowContext} from "../context-providers/ChildWindowProvider";
-
-const initialMessagesState: SampleMessage[] = [
-    {
-        id: 1,
-        sample_name: 'Greeting',
-        theme: 'Welcome',
-        message_text: 'Welcome to our service!',
-        recipient_type_id: 1,
-        media_path: '',
-        sending_date: Date.now()
-    },
-];
-
+import {useQuery} from "react-query";
 
 const TemplateMessages = () => {
-    const [templateMessages, setTemplateMessages] = useState(initialMessagesState);
-
+    const { data, isLoading, isError } = useQuery(
+        ['getSamples'],
+        () => getAllSampleMessages(),
+        {
+            keepPreviousData: true,
+        }
+    );
     const childWindow = useContext(ChildWindowContext);
 
     const handleOpenSample = (id:number) => childWindow?.openChildWindow({type: 'sample', id: id});
 
-    useEffect(() => {
-        getAllSampleMessages()
-            .then((response: SampleMessage[]) => {
-                setTemplateMessages(response);
-            })
-            .catch(error => {
-                console.error('Error fetching messenger types:', error);
-            });
-    }, []);
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error</div>;
+    if (!data) return <div>No data</div>;
+
     return (
-        templateMessages.map((item, index) => (
+        data.map((item, index) => (
             <div key={index} tabIndex={item.id} onClick={(e) => handleOpenSample(e.currentTarget.tabIndex)}
                  className="px-7 pt-1 cursor-pointer hover:border-0 hover:border-solid hover:border-cyan-800 hover:rounded-md hover:bg-cyan-800/80 hover:text-white ">
                 <div>{item.theme}</div>
